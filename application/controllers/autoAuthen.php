57ea41f5d6e4976fb8854387aa3cd3562525ccba
Tiến Trần Đình <tien.trandinh99@gmail.com>
2020-11-09 23:41:36 +0700

    update login form and login form controller

--- application/controllers/autoAuthen.php
+++ application/controllers/autoAuthen.php
@@ -3,20 +3,30 @@
 
     class AutoAuthen {
 
+        /**
+         * Remember user status when user choose 'Remember me'
+         *
+         * @param string $user name of user
+         */ 
         public static function loginWithRemember($user) {
             $token =  bin2hex(random_bytes(20));
             User::storeTokenForUser($user, $token);
             $cookie = $user . ':' . $token;
-            $mac = hash_hmac('sha256', $cookie, parse_ini_file('../../secret/config.ini')['secret_key']);
+            $mac = hash_hmac('sha256', $cookie, Config::get()['secret_key']);
             $cookie .= ':' . $mac;
             setcookie('rememberme', $cookie);
         }
 
+        /**
+         * Check if user have token (which mean the user choosed 'Remember me')
+         * 
+         * @return boolean true if the user has token
+         */ 
         public static function rememberMe() {
             $cookie = isset($_COOKIE['rememberme']) ? $_COOKIE['rememberme'] : '';
             if ($cookie != '') {
                 list ($user, $token, $mac) = explode(':', $cookie);
-                if (!hash_equals(hash_hmac('sha256', $user . ':' . $token, parse_ini_file('../../secret/config.ini')['secret_key']), $mac)) {
+                if (!hash_equals(hash_hmac('sha256', $user . ':' . $token, Config::get()['secret_key']), $mac)) {
                     return false;
                 }
                 $usertoken = User::getToken($user);
@@ -28,11 +38,15 @@
             return false;
         }
 
+        /**
+         * Login the user when recognize that the user has token
+         * 
+         * @param string $user name of user
+         */ 
         public static function autoLogin($user) {
             $_SESSION['user_type'] = User::getUserType($user);
             $_SESSION['user_id'] = User::getId($user);
             $_SESSION['username'] = $user;
-            error_log($_SESSION['username'].$_SESSION['user_id'].$_SESSION['user_type'], 3, '../my_errors.log');
         }
 
     }
--- application/controllers/infoTutor.php
+++ application/controllers/infoTutor.php
@@ -1,12 +1,21 @@
 <?php
     session_start();
+    include_once "../../secret/config.php";
     include_once "../models/tutor.php";
     include_once "../models/subject.php";
     include_once "../models/users.php";
-
+    
+    /**
+     * get info of tutor
+     *
+     * @param string $tutor_id id of the tutor, which saved in SESSION
+     * @param string $password pass of user
+     * 
+     * @return string status of authorization
+     */ 
     function getTutorInfo($tutor_id){
-        if (count(glob("../../static/images/avatar/" .$tutor_id .".*")) > 0){
-            $avatar_dir = glob("../../static/images/avatar/" .$tutor_id .".*")[0];
+        if (count(glob(Config::get()['avatar']['avatars_dir'] .$tutor_id .".*")) > 0){
+            $avatar_dir = glob(Config::get()['avatar']['avatars_dir']  .$tutor_id .".*")[0];
         } else {
             $avatar_dir = '';
         }
@@ -18,8 +27,15 @@
         echo (json_encode($response));     
     }
 
+    /**
+     * upadte info of tutor
+     *
+     * @param array $input_data all input update data (tutuor: last_name, first_name, date_of_birth, email, present_job, gender, phone_number, description)
+     *                                                (password) (speciality: array of speciality id)
+     * 
+     * @return string true: update sucess | false: fail when update
+     */ 
     function updateInfo($input_data) {
-        error_log('sadasdas',0,'../my_errors.log');
         $result_tutor = Tutor::updateTutor($_SESSION['user_id'], $input_data['tutor']);
         if (!empty($input_data['speciality'])) $result_speciality = Tutor::updateSpeciality($_SESSION['user_id'], $input_data['speciality']);
         else $result_speciality = Tutor::updateSpeciality($_SESSION['user_id'], []);
@@ -30,10 +46,12 @@
         echo (($result_tutor&&$result_speciality&&$result_pass) ? 'true' : 'false');
     }
 
+    //Check for get Tutor request
     if(isset($_GET['get_data_db']) && $_SESSION['user_type'] == 'tutor') {
         getTutorInfo($_SESSION['user_id']);
     }
 
+    //Check for update data request
     if(isset($_POST['changeData']) && $_SESSION['user_type'] == 'tutor') {
         updateInfo($_POST['changeData']);
     }
