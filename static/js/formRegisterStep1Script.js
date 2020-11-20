651dfda8ed1f7a5baececd5e4f1d991351c4ffc7
khoathecoolest <khoa.nguyen1001@hcmut.edu.vn>
2020-11-20 12:49:05 +0700

    Fix bug formRegister

--- static/js/formRegisterStep1Script.js
+++ static/js/formRegisterStep1Script.js
@@ -34,7 +34,8 @@ function resetNoti() {
     });
 }
 
-function chooseParent() {
+/*decorate the header tab; begin*/
+$('#tabH1').click(function tabHeader1() {
     if (document.getElementById("tabH1").classList.contains("active")) return;
     document.getElementById("tabH2").classList.remove("active");
     document.getElementById("tabH3").classList.remove("active");
@@ -47,10 +48,13 @@ function chooseParent() {
     document.querySelector("#tabH2").style.backgroundColor = "#28a6cc";
     document.querySelector("#tabH3").style.backgroundColor = "#28a6cc";
     resetAllData();
+    resetIntervalId(timerIntervalId);
+    emailButton();
+    $('#email_button').bind("click", ()=>sendUserMail());
     resetNoti();
-}
+});
 
-function chooseTutor() {
+$('#tabH2').click(function tabHeader2() {
     if (document.getElementById("tabH2").classList.contains("active")) return;
     document.getElementById("tabH1").classList.remove("active");
     document.getElementById("tabH3").classList.remove("active");
@@ -63,10 +67,13 @@ function chooseTutor() {
     document.querySelector("#tabH2").style.backgroundColor = "rgba(141, 213, 232, 0.5)";
     document.querySelector("#tabH3").style.backgroundColor = "#28a6cc";
     resetAllData();
+    resetIntervalId(timerIntervalId);
+    emailButton();
+    $('#email_button').bind("click", ()=>sendUserMail());
     resetNoti();
-}
+});
 
-function chooseAdmin() {
+$('#tabH3').click(function tabHeader3() {
     if (document.getElementById("tabH3").classList.contains("active")) return;
     document.getElementById("tabH1").classList.remove("active");
     document.getElementById("tabH2").classList.remove("active");
@@ -78,22 +85,131 @@ function chooseAdmin() {
     document.querySelector("#tabH2").style.backgroundColor = "#28a6cc";
     document.querySelector("#tabH3").style.backgroundColor = "rgba(75, 186, 220, 0.5)";
     resetAllData();
+    resetIntervalId(timerIntervalId);
+    emailButton();
+    $('#email_admin_button').bind("click", ()=>sendAdminMail());
     resetNoti();
-}
-
-/*decorate the header tab; begin*/
-$('#tabH1').click(function tabHeader1() {
-    chooseParent();
-});
-
-$('#tabH2').click(function tabHeader2() {
-    chooseTutor();
-});
-
-$('#tabH3').click(function tabHeader3() {
-    chooseAdmin();
 });
 /*decorate the header tab; end*/
+
+//Countdown button
+var timerIntervalId = null;
+var time_limit = 10;
+function resetIntervalId(timerIntervalId) {
+  clearInterval(timerIntervalId);
+}
+function resetTimer() {
+    let timePassed = 0;
+    let timeLeft = time_limit;
+    let timerInterval = null;
+    let remainingPathColor = 'green';
+    let template = `
+    <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+            id="base-timer-path-remaining"
+            stroke-dasharray="283"
+            class="base-timer__path-remaining ${remainingPathColor}"
+            d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+            "
+        ></path>
+        </g>
+    </svg>
+    <span id="base-timer-label" class="base-timer__label">${timeLeft}</span>
+    </div>
+    `;
+    if(document.getElementById('formParentAndTutor').classList.contains('active')) {
+        document.querySelector("#app-user").innerHTML = template;
+    }
+    else {
+        document.querySelector("#app-admin").innerHTML = template;
+    }
+    return [timePassed, time_limit, timeLeft];
+}
+
+function startTimer(timePassed, TIME_LIMIT, timeLeft) {
+    
+    timerIntervalId = setInterval(() => {
+    timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = timeLeft;
+    setCircleDasharray(timeLeft, TIME_LIMIT);
+    setRemainingPathColor(timeLeft);
+    if (timeLeft === 0) {
+        resetIntervalId(timerIntervalId);
+        emailButton();
+        if(document.getElementById('formParentAndTutor').classList.contains('active')) {
+            $('#email_button').bind("click", ()=>sendUserMail());
+        }
+        else {
+            $('#email_admin_button').bind("click", ()=>sendUserMail());
+        }
+    }
+  }, 1000);
+}
+
+function setRemainingPathColor(timeLeft) {
+    const WARNING_THRESHOLD = 10;
+    const ALERT_THRESHOLD = 5;
+    const COLOR_CODES = {
+        info: {
+            color: "green"
+        },
+        warning: {
+            color: "orange",
+            threshold: WARNING_THRESHOLD
+        },
+        alert: {
+            color: "red",
+            threshold: ALERT_THRESHOLD
+        }
+        };
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(warning.color);
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(info.color);
+        document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(warning.color);
+    }
+}
+
+function calculateTimeFraction(timeLeft, TIME_LIMIT) {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray(timeLeft, TIME_LIMIT) {
+  const circleDasharray = `${(
+    calculateTimeFraction(timeLeft, TIME_LIMIT) * 283
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+function emailButton() {
+    let button_user = `<button id="email_button" style="background-color: transparent;" type="button"><i class="fas fa-user-check"></i></button>`;
+    let button_admin = `<button id="email_admin_button" style="background-color: transparent;" type="button"><i class="fas fa-user-check"></i></button>`;
+    document.querySelector('#app-user').innerHTML = button_user;
+    document.querySelector('#app-admin').innerHTML = button_admin;
+    
+}
+//End button countdown
+
 
 $("#continue-admin").click(function continueButton() {
     var infoRegister = getAllInfo('admin');
@@ -147,8 +263,7 @@ $("#continue").click(function continueButton() {
         }
     });
 });
-
-$("#email_admin_button").click(function sendMail() {
+function sendAdminMail() {
     var email = document.getElementById("email-admin").value;
     if (email) {
         $.ajax({
@@ -164,9 +279,10 @@ $("#email_admin_button").click(function sendMail() {
             }
         });
     }
-});
-
-$("#email_button").click(function sendMail() {
+    let timeSetting = resetTimer();
+    startTimer(timeSetting[0], timeSetting[1], timeSetting[2]);
+}
+function sendUserMail() {
     var email = document.getElementById("email").value;
     if (email) {
         $.ajax({
@@ -182,16 +298,10 @@ $("#email_button").click(function sendMail() {
             }
         });
     }
-});
+    let timeSetting = resetTimer();
+    startTimer(timeSetting[0], timeSetting[1], timeSetting[2]);
+}
 
-$(document).ready(function checkRole() {
-    var role = localStorage.getItem("register-role");
-    if (role != null) {
-        if (role == 'tutor') {
-            chooseTutor();
-        } else if (role == 'parent') {
-            chooseParent();
-        }
-        localStorage.removeItem('register-role');
-    }
-});
+$("#email_admin_button").click(()=>sendAdminMail());
+
+$("#email_button").click(()=>sendUserMail());
