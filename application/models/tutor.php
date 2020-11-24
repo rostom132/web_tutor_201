b834c65e608e53d41488fccb5043c3af093faeb5
NhanNgocThien <nhanthien1012@gmail.com>
2020-11-24 14:26:18 +0700

    Delete error.log

--- application/models/tutor.php
+++ application/models/tutor.php
@@ -77,8 +77,6 @@
                     "INSERT INTO `specialize` (`tutor_id`, `subject_id`)
                     VALUES (" ."'$tutor_id','" .implode("'),('".$tutor_id ."','", $speciality) ."')"
                 );
-                error_log( "INSERT INTO `specialize` (`tutor_id`, `subject_id`)
-                VALUES (" ."'$tutor_id','" .implode("'),('".$tutor_id ."','", $speciality) ."')", 3, "./log.log");
             }
             if (!$result) return false;
             return true;
