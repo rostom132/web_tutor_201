cea6113ba3441509e9366876a15362bd210c5e6b
NhanNgocThien <nhanthien1012@gmail.com>
2020-11-24 12:48:33 +0700

    Fix ? url bug

--- static/js/filterForm.js
+++ static/js/filterForm.js
@@ -1,20 +1,19 @@
 import option from "./constant/filterValue.js";
 import * as constants from "./constant/city.js";
-import {getClassFiltered, initClass, filterClass, keepFilterValue, getURLParam} from "./getClass.js"
+import { getClassFiltered, initClass, filterClass, keepFilterValue, getURLParam } from "./getClass.js"
 
 //Call init class
-$(document).ready(function(){
-    if(getURLParam()['page']==null) {
-        window.location.replace(window.location.href+'?page=1');
-    }
-    else {
+$(document).ready(function() {
+    if (getURLParam()['page'] == null) {
+        window.location.replace(window.location.origin + window.location.pathname + '?page=1');
+    } else {
         //Render district 
         var optgroup = document.getElementById("district");
-        $.each(constants.cityData.districts, function(index, element){
-        var opt = document.createElement('option')
-        opt.value = element.id;
-        opt.text = element.name;
-        optgroup.appendChild(opt);
+        $.each(constants.cityData.districts, function(index, element) {
+            var opt = document.createElement('option')
+            opt.value = element.id;
+            opt.text = element.name;
+            optgroup.appendChild(opt);
         });
         //Render subject
         optgroup = document.getElementById("subject");
@@ -29,31 +28,29 @@ $(document).ready(function(){
         for (let i in option.gender) {
             var opt = document.createElement('option')
             opt.text = option.gender[i];
-            if(option.gender[i] == "Male") {
+            if (option.gender[i] == "Male") {
                 opt.value = "M";
             }
-            if(option.gender[i] == "Female") {
+            if (option.gender[i] == "Female") {
                 opt.value = "F";
             }
-            if(option.gender[i] == "Both") {
+            if (option.gender[i] == "Both") {
                 opt.value = "B";
             }
-            
+
             optgroup.appendChild(opt);
         }
         keepFilterValue();
         let param = getURLParam();
-        if(param['gender']==null||param['dist']==null||param['sub']==null) {
+        if (param['gender'] == null || param['dist'] == null || param['sub'] == null) {
             initClass();
-        }
-        else {
-            filterClass($("#edit-place").val(),$("#edit-subject").val(),$("#edit-gender").val());
+        } else {
+            filterClass($("#edit-place").val(), $("#edit-subject").val(), $("#edit-gender").val());
         }
     }
 })
 
 //Function to run after submit filter
-$("#submit-button").click(function(){
-    window.location.replace(window.location.origin+window.location.pathname+"?page=1"+"&dist="+$("#edit-place").val()+"&sub="+$("#edit-subject").val()+"&gender="+$("#edit-gender").val());
+$("#submit-button").click(function() {
+    window.location.replace(window.location.origin + window.location.pathname + "?page=1" + "&dist=" + $("#edit-place").val() + "&sub=" + $("#edit-subject").val() + "&gender=" + $("#edit-gender").val());
 });
-    
