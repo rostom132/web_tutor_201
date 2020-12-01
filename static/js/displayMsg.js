d37506bd4d88bf153b9fc34040a6d7a42bb7d531
NhanNgocThien <nhanthien1012@gmail.com>
2020-12-01 17:33:42 +0700

    Add callback function to OK button alert

--- static/js/displayMsg.js
+++ static/js/displayMsg.js
@@ -1,9 +1,16 @@
-window.alert = function(message) {
+window.alert = function(message, callback) {
     $(document.createElement('div'))
         .attr({ title: 'Notification' })
         .html(message)
         .dialog({
-            buttons: { OK: function() { $(this).dialog('close'); } },
+            buttons: {
+                OK: function() {
+                    if (jQuery.isFunction(callback)) {
+                        $(this).dialog('close');
+                        callback();
+                    }
+                }
+            },
             close: function() { $(this).remove(); },
             draggable: true,
             modal: true,
